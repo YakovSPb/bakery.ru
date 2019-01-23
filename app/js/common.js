a8e17a22833d5b3ffe3ee43a4e7b19bb6278f6a5
Yakov <yakov_dux@bk.ru>
2019-01-24 02:36:44 +0400

    Menu is ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
 $(function() {
 
-	$('#my-menu').mmenu();
+
+    var windowWidth = $(window).width();
+    if(windowWidth < 1200){
+
+        $('#my-menu').mmenu({
+            extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+            navbar: {
+                title: '<div class="logo">Gustoso</div>'
+            }
+        });
+    }
+
+    var api = $("#my-menu").data("mmenu");
+    api.bind('open:finish', function () {
+        $('.hamburger').toggleClass('is-active');
+    }).bind('close:finish', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+
+
+
 
 });
