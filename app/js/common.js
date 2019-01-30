c5673b079c44a7e1aef9d91f0cedea142e5b2112
YakovSPb <prospektpro673@gmail.com>
2019-01-30 13:46:23 +0400

    Slider is ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,93 @@
 $(function() {
 
 
-    var windowWidth = $(window).width();
-    if(windowWidth < 1200){
+//SLIDER//
 
+
+$('.slider2').owlCarousel({
+    loop : true,
+    items: 3,
+    itemClass: "slide-wrap",
+    nav: true,
+    dots: false
+});
+
+
+$('.on1').click(function(){
+    
+  $('.food-image img').removeClass('on');
+
+  $('.food-image img:nth-child(1)').addClass('on'); 
+
+  $('.food-text').removeClass('ton');
+  $('.food-text:nth-child(1)').addClass('ton');
+});
+
+
+$('.on2').click(function(){
+    
+  $('.food-image img').removeClass('on');
+
+  $('.food-image img:nth-child(2)').addClass('on');
+
+  $('.food-text').removeClass('ton');
+  $('.food-text:nth-child(2)').addClass('ton');
+});
+
+
+$('.on3').click(function(){
+    
+  $('.food-image img').removeClass('on');
+
+  $('.food-image img:nth-child(3)').addClass('on');
+  $('.food-text').removeClass('ton');
+  $('.food-text:nth-child(3)').addClass('ton');
+});
+
+
+$('.on4').click(function(){
+    
+  $('.food-image img').removeClass('on');
+
+  $('.food-image img:nth-child(4)').addClass('on');
+  $('.food-text').removeClass('ton');
+  $('.food-text:nth-child(4)').addClass('ton');
+});
+
+
+    // $('#on2').click(function(){
+        
+    //       $('.food-image img').removeClass('on');
+
+    //     $('.food-image img:nth-child(1)').addClass('on');
+    // });
+
+
+//MENU//
+
+var api = $("#my-menu").data("mmenu");
+api.bind('open:finish', function () {
+    $('.hamburger').toggleClass('is-active');
+}).bind('close:finish', function () {
+    $('.hamburger').removeClass('is-active');
+}); 
+});
+
+$(function(){
+    if ($(window).width() < 500){
+        // Подключаем стиль для мобильных
+        $("head").append($("<link rel='stylesheet' href='css/styleMob.css' type='text/css' media='screen' />"));
+    }
+    else{
+        // Подключаем стиль для остальных
+        $("head").append($("<link rel='stylesheet' href='css/style.css' type='text/css' media='screen' />"));
+    }
+});
+
+
+
+$(window).resize(function() {
+    if(document.documentElement.clientWidth < 1200) {
         $('#my-menu').mmenu({
             extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
             navbar: {
@@ -11,15 +95,5 @@ $(function() {
             }
         });
     }
-
-    var api = $("#my-menu").data("mmenu");
-    api.bind('open:finish', function () {
-        $('.hamburger').toggleClass('is-active');
-    }).bind('close:finish', function () {
-        $('.hamburger').removeClass('is-active');
-    });
-
-
-
-
 });
+
